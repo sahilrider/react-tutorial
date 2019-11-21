6a7faa51996d1fa3340105ff80be5edbc294145e
sahilrider <sahilriders@gmail.com>
2019-11-21 23:47:23 +0530

    Updates

---
+++ precache-manifest.67c37d0c1a70d37cd76ba88e2d191cfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a61ada19283a3a625b717f21bef82b9",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "0ad73fd8f18e1b5423e0",
+    "url": "/react-tutorial/static/css/main.b8765e77.chunk.css"
+  },
+  {
+    "revision": "6fea7dec174f8202d156",
+    "url": "/react-tutorial/static/js/2.4734f653.chunk.js"
+  },
+  {
+    "revision": "0ad73fd8f18e1b5423e0",
+    "url": "/react-tutorial/static/js/main.c9e94ff9.chunk.js"
+  },
+  {
+    "revision": "23cd54c0b15ae01a9f16",
+    "url": "/react-tutorial/static/js/runtime-main.09266777.js"
+  }
+]);
